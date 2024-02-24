51f53a44e52a04cf7b008e997cbed7cd14625497
Tirusew Ayenew Cheru <133959484+tirusew-7@users.noreply.github.com>
2024-02-24 12:41:38 +0300

    Adding database_utils and Data page

--- Pages/1_Data.py
+++ Pages/1_Data.py
@@ -1,46 +1,14 @@
-# Import necessary libraries
+# 1_Data.py
+
 import streamlit as st
-import pandas as pd
-from sklearn.preprocessing import LabelEncoder
-import pyodbc
-from streamlit_secrets import get_secret
+from database_utils import read_data
 
-# Function to fetch data from SQL Server
-def fetch_data():
-    # Fetch credentials from Streamlit secrets
-    secrets = get_secret("my_secrets")
+# Example SQL query
+sql_query = "SELECT * FROM LP2_Telco_churn_first_3000;"
 
-    # Connection details
-    conn_str = f"DRIVER=ODBC Driver 17 for SQL Server;SERVER={secrets['servername']};DATABASE={secrets['databasename']};UID={secrets['user']};PWD={secrets['password']}"
-    
-    # SQL Query to fetch data from the specified table
-    query = f"SELECT * FROM {secrets['tablename']}"
+# Use the read_data function to fetch data from the database
+result_df = read_data(sql_query)
 
-    # Fetch data from SQL Server and convert to DataFrame
-    conn = pyodbc.connect(conn_str)
-    cursor = conn.cursor()
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    df = pd.DataFrame([tuple(row) for row in rows], columns=[column[0] for column in cursor.description])
-    conn.close()
-
-    # Encode categorical variables using LabelEncoder
-    le = LabelEncoder()
-    for column in df.select_dtypes(include=['object']).columns:
-        df[column] = le.fit_transform(df[column])
-
-    return df
-
-# Create Data Page
-def data_page():
-    st.title("Data Page")
-
-    # Fetch data from SQL Server
-    data = fetch_data()
-
-    # Display the fetched data
-    st.dataframe(data)
-
-# Run the app
-if __name__ == "__main__":
-    data_page()
+# Display the data in the Streamlit app
+st.write("Data from the database:")
+st.write(result_df)
