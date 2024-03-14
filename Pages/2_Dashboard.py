69a60b385ad15afa4ce1a46e368de86748214d90
brianbassey37 <brianbassey37@gmail.com>
2024-03-14 09:10:15 +0100

    login error

--- Pages/2_Dashboard.py
+++ Pages/2_Dashboard.py
@@ -23,7 +23,7 @@ st.set_page_config(
     page_icon='',
     layout='wide'
 )
-
+df_train = pd.read_csv('Dataset\Cleaned_data.csv')
 # Check if the user is logged in
 if 'name' not in st.session_state:
     st.sidebar.title("Login/Create Account")
@@ -51,22 +51,19 @@ else:
     # Authenticate user login
     username = st.session_state['name']
     password = st.text_input("Password", type="password")
-    if authenticate(username, password):
-        st.success("Login successful.")
-        file_path = '.Dataset\Telco-churn-last-2000.xlsx'
-        # Function to load CSV data
-        @st.cache
-        def load_csv_data(file_path):
-            try:
-                df = pd.read_excel(file_path)
-                return df
-            except Exception as e:
-                st.error(f"Error loading CSV file: {e}")
+
+    @st.cache
+    def load_csv_data(df_train):
+        try:
+            df = pd.read_excel(df_train)
+            return df
+        except Exception as e:
+            st.error(f"Error loading CSV file: {e}")
 
         # Function to show the About Dashboard section
-        def show_about_dashboard():
-            st.markdown("### About Dashboard")
-            st.write('''
+    def show_about_dashboard():
+        st.markdown("### About Dashboard")
+        st.write('''
              This dashboard provides insights into Telco Churn data. 
              - Explores different visualizations and key performance indicators 
              - Helps to gain valuable information.
@@ -85,15 +82,15 @@ else:
              insights into the dataset, aiding in decision-making and strategy development.''')
         
         # Function to show the Key Performance Indicators section
-        def show_kpis(df_train):
-            st.markdown("### Key Performance Indicators")
-            st.subheader("Customer Demographics")
-            st.write(f"Total Customers: {df_train.shape[0]}")
-            st.write(f"Total Churned Customers: {df_train['Churn'].value_counts()[1]}")
-            st.write(f"Total Non-Churned Customers: {df_train['Churn'].value_counts()[0]}")
-            st.write(f"Churn Rate: {df_train['Churn'].value_counts()[1] / df_train.shape[0] * 100:.2f}%")
-            st.write(f"Average Age: {df_train['SeniorCitizen'].mean():.2f}")
-            
+    def show_kpis(df_train):
+        st.markdown("### Key Performance Indicators")
+        st.subheader("Customer Demographics")
+        st.write(f"Total Customers: {df_train.shape[0]}")
+        st.write(f"Total Churned Customers: {df_train['Churn'].value_counts()[1]}")
+        st.write(f"Total Non-Churned Customers: {df_train['Churn'].value_counts()[0]}")
+        st.write(f"Churn Rate: {df_train['Churn'].value_counts()[1] / df_train.shape[0] * 100:.2f}%")
+        st.write(f"Number of Senior Citizens: {len(df_train[df_train['SeniorCitizen'] == 'Yes'])}")
+    
         # calculate overall KPIs
         avg_tenure = df_train['Tenure'].mean()
         avg_monthly_charges = df_train['MonthlyCharges'].mean()
@@ -102,73 +99,70 @@ else:
         total_total_charges = df_train['TotalCharges'].sum()
         
         # Function to show the Explore section
-        def show_explore(df_train):
-            st.markdown("### Exploratory Data Analysis ")
-            st.sidebar.subheader("Filter among the following Options")
+    def show_explore(df_train):
+        st.markdown("### Exploratory Data Analysis ")
+        st.sidebar.subheader("Filter among the following Options")
 
-            numerical_columns = ['Tenure', 'MonthlyCharges', 'TotalCharges']
-            categorical_columns = ['Gender', 'SeniorCitizen', 'PhoneService', 'StreamingMovies',
-                                    'Contract', 'PaperlessBilling', 'PaymentMethod', 'Churn']
+        numerical_columns = ['Tenure', 'MonthlyCharges', 'TotalCharges']
+        categorical_columns = ['Gender', 'SeniorCitizen', 'PhoneService', 'StreamingMovies',
+                                'Contract', 'PaperlessBilling', 'PaymentMethod', 'Churn']
 
-            for column in categorical_columns:
-                if st.sidebar.checkbox(f'{column} Distribution'):
-                    column_counts = df_train[column].value_counts()
-                    if len(column_counts) <= 2:
-                        # Bar chart for few unique values
-                        fig, ax = plt.subplots()
-                        ax.bar(column_counts.index, column_counts, color=['#66c2a5', '#fc8d62'])
-                        ax.set_title(f'Distribution of {column}')
-                        ax.set_xlabel(column)
-                        ax.set_ylabel('Count')
-                        st.pyplot(fig)
-                    else:
-                        # Pie chart for more unique values
-                        fig, ax = plt.subplots()
-                        ax.pie(column_counts, labels=column_counts.index, autopct='%1.1f%%',
-                               startangle=90, colors=sns.color_palette('pastel'))
-                        ax.set_title(f'Distribution of {column}')
-                        st.pyplot(fig)
-
-            for column in numerical_columns:
-                if st.sidebar.checkbox(f'{column} Distribution'):
-                    # Histogram for numerical values
-                    num_bins = st.slider(f'Select number of bins for {column}', min_value=5, max_value=50, value=20)
+        for column in categorical_columns:
+            if st.sidebar.checkbox(f'{column} Distribution'):
+                column_counts = df_train[column].value_counts()
+                if len(column_counts) <= 2:
+                    # Bar chart for few unique values
                     fig, ax = plt.subplots()
-                    ax.hist(df_train[column], bins=num_bins, color='#8da0cb')
+                    ax.bar(column_counts.index, column_counts, color=['#66c2a5', '#fc8d62'])
                     ax.set_title(f'Distribution of {column}')
                     ax.set_xlabel(column)
                     ax.set_ylabel('Count')
                     st.pyplot(fig)
-
-                if st.sidebar.checkbox(f'{column} vs Churn'):
+                else:
+                    # Pie chart for more unique values
                     fig, ax = plt.subplots()
-                    sns.histplot(data=df_train, x=column, hue='Churn', multiple='stack', bins=num_bins, kde=True,
-                                 palette=['#66c2a5', '#fc8d62'])
-                    ax.set_title(f'{column} distribution by Churn')
-                    ax.set_xlabel(column)
-                    ax.set_ylabel('Count')
+                    ax.pie(column_counts, labels=column_counts.index, autopct='%1.1f%%',
+                           startangle=90, colors=sns.color_palette('pastel'))
+                    ax.set_title(f'Distribution of {column}')
                     st.pyplot(fig)
 
-            if st.sidebar.checkbox('Correlation Matrix'):
-                # Correlation heatmap for numerical columns
-                numerical_data = df_train[numerical_columns]
-                correlation_matrix = numerical_data.corr()
-
-                fig, ax = plt.subplots(figsize=(8, 8))
-                sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=.5, fmt=".2f")
-                ax.set_title('Correlation Heatmap for Numerical Columns')
+        for column in numerical_columns:
+            if st.sidebar.checkbox(f'{column} Distribution'):
+                    # Histogram for numerical values
+                num_bins = st.slider(f'Select number of bins for {column}', min_value=5, max_value=50, value=20)
+                fig, ax = plt.subplots()
+                ax.hist(df_train[column], bins=num_bins, color='#8da0cb')
+                ax.set_title(f'Distribution of {column}')
+                ax.set_xlabel(column)
+                ax.set_ylabel('Count')
                 st.pyplot(fig)
 
-        # Load the dataset
-        df_train = pd.read_csv('./Dataset/Cleaned_data.csv')
+            if st.sidebar.checkbox(f'{column} vs Churn'):
+                fig, ax = plt.subplots()
+                sns.histplot(data=df_train, x=column, hue='Churn', multiple='stack', bins=num_bins, kde=True,
+                            palette=['#66c2a5', '#fc8d62'])
+                ax.set_title(f'{column} distribution by Churn')
+                ax.set_xlabel(column)
+                ax.set_ylabel('Count')
+                st.pyplot(fig)
+
+        if st.sidebar.checkbox('Correlation Matrix'):
+                # Correlation heatmap for numerical columns
+            numerical_data = df_train[numerical_columns]
+            correlation_matrix = numerical_data.corr()
+
+            fig, ax = plt.subplots(figsize=(8, 8))
+            sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=.5, fmt=".2f")
+            ax.set_title('Correlation Heatmap for Numerical Columns')
+            st.pyplot(fig)
 
         # Sidebar Navigation
-        sidebar_selection = st.sidebar.radio("Select Section", ["About Dashboard", "Explore", "Key Performance Indicators"])
+    sidebar_selection = st.sidebar.radio("Select Section", ["About Dashboard", "Explore", "Key Performance Indicators"])
         
         # Display the selected section
-        if sidebar_selection == "About Dashboard":
-            show_about_dashboard()
-        elif sidebar_selection == "Explore":
-            show_explore(df_train)
-        else:
-            show_kpis(df_train)
+    if sidebar_selection == "About Dashboard":
+        show_about_dashboard()
+    elif sidebar_selection == "Explore":
+        show_explore(df_train)
+    else:
+        show_kpis(df_train)
